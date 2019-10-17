19b9e0f340283a46b2cd9d7c884aa7f87c36ea9c
Animesh <animeshg93@utexas.edu>
2019-10-17 00:36:12 -0700

    Second commt

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Hello World</h1>
+      <p>React with flux tutorial</p>
+    </div>
+  );
+}
+
+export default HomePage;
