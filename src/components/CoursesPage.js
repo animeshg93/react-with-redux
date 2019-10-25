8eac0f5f573e5879a5c1f886d1e86fa0f3e819bf
Animesh <animeshg93@utexas.edu>
2019-10-25 00:14:28 -0700

    Added more components with props and state

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CourseList from "./CourseList";
 import { getCourses } from "../api/courseApi";
-import { thisExpression } from "@babel/types";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: []
-  };
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
 
-  componentDidMount() {
-    getCourses().then(courses => this.setState({ courses: courses }));
-  }
+  useEffect(() => {
+    getCourses().then(_courses => setCourses(_courses));
+  }, []);
 
-  render() {
-    return <h2>Courses</h2>;
-  }
+  return (
+    <>
+      <h2>Courses</h2>
+      <CourseList courses={courses} />
+    </>
+  );
 }
 
 export default CoursesPage;
