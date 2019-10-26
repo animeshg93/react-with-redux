081ab2503134fe06411ec6ecdb2ac03a40081129
Animesh <animeshg93@utexas.edu>
2019-10-26 02:23:25 -0700

    Added 404 page and Link for clien-side routing

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="jumbotron">
       <h1>Hello World</h1>
       <p>React with flux tutorial</p>
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
     </div>
   );
 }
