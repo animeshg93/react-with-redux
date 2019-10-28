16502279d04f52e138a90b8cbc9255519bf3b5e8
Animesh <animeshg93@utexas.edu>
2019-10-28 00:25:06 -0700

    Added Form controls

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
+import ManageCoursePage from "./ManageCoursePage";
 import NotFoundPage from "./NotFoundPage";
 import Header from "./common/Header";
 import { Route, Switch, Redirect } from "react-router-dom";
@@ -14,6 +15,8 @@ function App() {
         <Route path="/" exact component={HomePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Redirect from="/about-page" to="about" />
         <Route component={NotFoundPage} />
       </Switch>
---
+++ src/components/ManageCoursePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CourseForm from "./CourseForm";
+
+const ManageCoursePage = props => {
+  return (
+    <>
+      <h2>Manage Courses</h2>
+      <CourseForm />
+    </>
+  );
+};
+
+export default ManageCoursePage;
